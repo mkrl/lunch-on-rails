62739345f710162aa9c97c2748aabb9d9efe309d
Mikhail <tradecontent@gmail.com>
2018-01-22 23:57:21 +0800

    Removed old workers controller

--- test/controllers/workers_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WorkersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get workers_new_url
-    assert_response :success
-  end
-
-end
