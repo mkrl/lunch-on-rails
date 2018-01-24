6a85cacaa3faea63643841308286ed8914ff667a
Mikhail <tradecontent@gmail.com>
2018-01-24 17:28:43 +0800

    Removed session aliases causing incorrect root_url path

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   get 'workers/new'
   
   get 'signup', to: 'workers#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   get 'home/index'
  
